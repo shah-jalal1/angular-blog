d56761239a41db56caac68b450b92566d2ad1153
shahjalalcse <shah-jalal1>
2021-10-12 13:04:10 +0600

    post model

---
+++ src/app/admin/admin/post-model.ts
@@ -0,0 +1,6 @@
+export class EmployeeModel{
+    postTitle : string = '';
+    postAuthor : string = '';
+    postSlug : string = '';
+    postDescription : string = '';
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,7 +16,8 @@ import { AdminComponent } from './admin/admin/admin.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
